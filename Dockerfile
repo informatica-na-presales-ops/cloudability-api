c19e0832b6b549ce4e1fc4d0fcb5655464ca1588
William Jackson <william@subtlecoolness.com>
2020-04-28 13:04:37 -0500

    Support multiple vendor ids

--- Dockerfile
+++ Dockerfile
@@ -7,15 +7,7 @@ RUN /usr/local/bin/pip install --no-cache-dir --requirement /cloudability-etl/re
 COPY cloudability-etl.py /cloudability-etl/cloudability-etl.py
 
 ENV APP_VERSION-="0.1.0" \
-    CLOUDABILITY_AUTH_TOKEN="" \
-    LOG_FORMAT="%(levelname)s [%(name)s] %(message)s" \
-    LOG_LEVEL="INFO" \
-    OUTPUT_FILE="/data/cloudability-daily-spend.csv" \
-    PYTHONUNBUFFERED="1" \
-    RETRY_COUNT="30" \
-    RETRY_INTERVAL="10" \
-    START_DATE="2019-01-01" \
-    VENDOR_ACCOUNT_ID=""
+    PYTHONUNBUFFERED="1"
 
 ENTRYPOINT ["/usr/local/bin/python"]
 CMD ["/cloudability-etl/cloudability-etl.py"]
--- cloudability-etl.py
+++ cloudability-etl.py
@@ -6,13 +6,57 @@ import logging
 import os
 import pathlib
 import requests
+import requests.auth
 import sys
 import time
 import urllib.parse
 
+from typing import List
+
 log = logging.getLogger(__name__)
-if __name__ == '__main__':
-    log = logging.getLogger('cloudability-sync')
+
+
+class Settings:
+    @property
+    def basic_auth(self) -> requests.auth.HTTPBasicAuth:
+        return requests.auth.HTTPBasicAuth(self.cloudability_auth_token, '')
+
+    @property
+    def cloudability_auth_token(self) -> str:
+        return os.getenv('CLOUDABILITY_AUTH_TOKEN')
+
+    @property
+    def log_format(self) -> str:
+        return os.getenv('LOG_FORMAT', '%(levelname)s [%(name)s] %(message)s')
+
+    @property
+    def log_level(self) -> str:
+        return os.getenv('LOG_LEVEL', 'INFO')
+
+    @property
+    def output_file(self) -> pathlib.Path:
+        return pathlib.Path(os.getenv('OUTPUT_FILE', '/data/cloudability-daily-spend.csv')).resolve()
+
+    @property
+    def start_date(self) -> datetime.date:
+        return datetime.datetime.strptime(os.getenv('START_DATE'), '%Y-%m-%d').date()
+
+    @property
+    def vendor_account_id(self) -> str:
+        return os.getenv('VENDOR_ACCOUNT_ID')
+
+    @property
+    def vendor_accounts(self) -> List:
+        value = []
+        raw = os.getenv('VENDOR_ACCOUNTS').split()
+        for r in raw:
+            vendor_id, vendor_name = r.split(':', maxsplit=1)
+            value.append({'vendor_id': vendor_id, 'vendor_name': vendor_name})
+        return value
+
+    @property
+    def version(self) -> str:
+        return os.getenv('APP_VERSION', 'unknown')
 
 
 def last_day_of_month(day: datetime.date) -> datetime.date:
@@ -26,74 +70,80 @@ def clean_currency(value: str) -> decimal.Decimal:
     return decimal.Decimal(value)
 
 
-def get_data():
+def get_data(settings: Settings):
     s = requests.Session()
     base_url = 'https://app.cloudability.com/api/1/reporting/cost'
-    token_only = {'auth_token': os.getenv('CLOUDABILITY_AUTH_TOKEN')}
+    token_only = {'auth_token': settings.cloudability_auth_token}
     query = {
-        'auth_token': os.getenv('CLOUDABILITY_AUTH_TOKEN'),
+        'auth_token': settings.cloudability_auth_token,
         'dimensions': 'resource_identifier,enhanced_service_name,tag1,tag13,date',
-        'filters': f'tag13!=(not set),vendor_account_identifier=={os.getenv("VENDOR_ACCOUNT_ID")}',
         'metrics': 'unblended_cost,adjusted_cost,usage_hours,usage_quantity'
     }
-    env_start_date = datetime.datetime.strptime(os.getenv('START_DATE'), '%Y-%m-%d').date()
-    start_date = env_start_date.replace(day=1)
-    while start_date < datetime.date.today():
-        log.info(f'Requesting data for month beginning {start_date}')
-        end_date = last_day_of_month(start_date)
-        query['start_date'] = str(start_date)
-        query['end_date'] = str(end_date)
-        url = f'{base_url}/enqueue?{urllib.parse.urlencode(query)}'
-        resp = s.get(url).json()
-        job_id = resp.get('id')
-        log.info(f'Request submitted, job {job_id}')
-        job_status = 'requested'
-        while not job_status == 'finished':
-            time.sleep(10)
-            url = f'{base_url}/reports/{job_id}/state?{urllib.parse.urlencode(token_only)}'
-            resp = s.get(url).json()
-            job_status = resp.get('status')
-            log.info(f'Job {job_id} is {job_status}')
-        log.info(f'Fetching results for job {job_id}')
-        url = f'{base_url}/reports/{job_id}/results?{urllib.parse.urlencode(token_only)}'
-        resp = s.get(url)
-        log.info(resp.text)
+    for vendor in settings.vendor_accounts:
+        start_date = settings.start_date.replace(day=1)
+        vendor_id = vendor.get('vendor_id')
+        query['filters'] = f'tag13!=(not set),vendor_account_identifier=={vendor_id}'
+        while start_date < datetime.date.today():
+            end_date = start_date + datetime.timedelta(days=7)
+            log.info(f'Requesting data from {start_date} to {end_date} for {vendor_id}')
+            query['start_date'] = str(start_date)
+            query['end_date'] = str(end_date)
+            url = f'{base_url}/enqueue?{urllib.parse.urlencode(query)}'
+            enqueue_response = s.get(url)
+            enqueue_response.raise_for_status()
+            enqueue_data = enqueue_response.json()
+            job_id = enqueue_data.get('id')
+            log.info(f'Request submitted, job {job_id}')
 
-        for result in resp.json().get('results'):
-            yield {
-                'resource_id': result.get('resource_identifier'),
-                'service_name': result.get('enhanced_service_name'),
-                'name': result.get('tag1'),
-                'owner_email': result.get('tag13'),
-                'date': result.get('date'),
-                'unblended_cost': clean_currency(result.get('unblended_cost')),
-                'adjusted_cost': clean_currency(result.get('adjusted_cost')),
-                'usage_hours': decimal.Decimal(result.get('usage_hours')),
-                'usage_quantity': decimal.Decimal(result.get('usage_quantity'))
-            }
-        start_date = end_date + datetime.timedelta(days=1)
+            job_status = 'requested'
+            while not job_status == 'finished':
+                time.sleep(10)
+                url = f'{base_url}/reports/{job_id}/state?{urllib.parse.urlencode(token_only)}'
+                state_response = s.get(url)
+                state_response.raise_for_status()
+                state_data = state_response.json()
+                job_status = state_data.get('status')
+                log.info(f'Job {job_id} is {job_status}')
+
+            log.info(f'Fetching results for job {job_id}')
+            url = f'{base_url}/reports/{job_id}/results?{urllib.parse.urlencode(token_only)}'
+            results_response = s.get(url)
+            results_response.raise_for_status()
+
+            for result in results_response.json().get('results'):
+                yield {
+                    'vendor_id': vendor_id,
+                    'vendor_name': vendor.get('vendor_name'),
+                    'resource_id': result.get('resource_identifier'),
+                    'service_name': result.get('enhanced_service_name'),
+                    'name': result.get('tag1'),
+                    'owner_email': result.get('tag13'),
+                    'date': result.get('date'),
+                    'unblended_cost': clean_currency(result.get('unblended_cost')),
+                    'adjusted_cost': clean_currency(result.get('adjusted_cost')),
+                    'usage_hours': decimal.Decimal(result.get('usage_hours')),
+                    'usage_quantity': decimal.Decimal(result.get('usage_quantity'))
+                }
+            start_date = end_date + datetime.timedelta(days=1)
 
 
 def main():
-    log_format = os.getenv('LOG_FORMAT')
-    log_level = os.getenv('LOG_LEVEL')
-    logging.basicConfig(format=log_format, level='DEBUG', stream=sys.stdout)
-    version = os.getenv('APP_VERSION', 'unknown')
-    log.debug(f'cloudability-etl {version}')
-    if not log_level == 'DEBUG':
-        log.debug(f'Setting log level to {log_level}')
-    logging.getLogger().setLevel(log_level)
+    settings = Settings()
+    logging.basicConfig(format=settings.log_format, level='DEBUG', stream=sys.stdout)
+    log.debug(f'cloudability-etl {settings.version}')
+    if not settings.log_level == 'DEBUG':
+        log.debug(f'Setting log level to {settings.log_level}')
+    logging.getLogger().setLevel(settings.log_level)
 
     csv_field_names = [
-        'resource_id', 'service_name', 'name', 'owner_email', 'date', 'unblended_cost', 'adjusted_cost', 'usage_hours',
-        'usage_quantity'
+        'vendor_id', 'vendor_name', 'resource_id', 'service_name', 'name', 'owner_email', 'date', 'unblended_cost',
+        'adjusted_cost', 'usage_hours', 'usage_quantity'
     ]
-    output_file = pathlib.Path(os.getenv('OUTPUT_FILE')).resolve()
-    log.info(f'Writing data to {output_file}')
-    with output_file.open('w', newline='') as f:
+    log.info(f'Writing data to {settings.output_file}')
+    with settings.output_file.open('w', newline='') as f:
         writer = csv.DictWriter(f, fieldnames=csv_field_names)
         writer.writeheader()
-        for row in get_data():
+        for row in get_data(settings):
             writer.writerow(row)
 
 
